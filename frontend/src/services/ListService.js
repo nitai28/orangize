623a8632f9b2cc72a95813a1412c45f0f18cac0d
yotamZaken <yotamzk@gmail.com>
2018-05-02 19:10:54 +0300

    frnotend list service added

---
+++ frontend/src/services/ListService.js
@@ -0,0 +1,44 @@
+const LIST_URL = '/list';
+
+function emptyList() {
+    return {
+        _id : ObjectId('5ae9c24dafaf4a13784ba582'),
+        title : 'Some Sort of List',
+        items : []
+    }
+}
+
+function getLists() {
+    return axios
+            .get(LIST_URL)
+            .then(res => res.data)
+            .catch(e => console.log('No Lists', e))
+}
+
+function saveList(list) {
+    if (list._id) return axios.put(_getListUrl(list._id), list)
+    else return axios.post(LIST_URL, list);  
+}
+
+function deleteList(listId) {
+    return axios.delete(_getListUrl(listId))
+}
+
+
+function getListById(listId) {
+    return axios
+    .get(_getListUrl(listId))
+    .then(res => res.data)
+}
+
+function _getListUrl(listId) {
+    return `${LIST_URL}/${listId}`;
+}
+
+export default {
+    getLists,
+    saveList,
+    deleteList,
+    emptyList,
+    getListById
+}
