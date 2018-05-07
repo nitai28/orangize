c4fd154b2ec66c0ac97de644b2082c258ba4a883
Taytay884 <Taytay884@yahoo.com>
2018-05-07 12:19:14 +0300

    Put the nav-bar on component, and made some CSS

--- frontend/src/components/global/NavBar.vue
+++ frontend/src/components/global/NavBar.vue
@@ -1,37 +1,51 @@
 <template>
-  <section class="nav-bar">
-        <router-link to="chat">Chat</router-link>
-        <router-link v-if="!loggedinUser" to="register">Register</router-link>
-        <router-link v-if="!loggedinUser" to="login">Login</router-link>
-        <router-link v-if="loggedinUser" to="admin">Admin</router-link>
-        <router-link v-if="loggedinUser" to="dog">Dogs App</router-link>
-        <button v-if="loggedinUser" @click="logout">{{loggedinUser.name}} Logout</button>
-    </section>
+  <section class="nav-bar flex space-around align-center justify-center">
+    <router-link to="/">Home</router-link>
+    <router-link to="/orangize">Orangize</router-link>
+    <div class="flex">
+    <i class="orangize"></i> <span>Orangize</span>
+    </div>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/register">Register</router-link>
+    <!-- <router-link to="/registerTest">RegisterTest</router-link> -->
+  </section>
 </template>
 
 <script>
 export default {
-  methods: {
-    logout() {
-      console.log("Logging out!");
-      this.$store.dispatch("logout").then(() => {
-        this.$router.push("/");
-      });
-    }
-  },
   computed: {
-    loggedinUser() {
-      return this.$store.getters.loggedinUser;
-    }
   }
 };
 </script>
 
-<style>
+<style scoped>
+/* 
+Black: #231f20
+LightOrange: #f26531
+WHITE: #f5f5f5
+*/
 
-</style>
-
-
-export default {
-   
+.nav-bar {
+  background: #231f20;
+  height: 40px;
+  box-shadow: 0px 0px 4px 2px #000000c4;
 }
+a {
+  /* display:block; */
+  color: #f26531;
+  transition: color .2s ease-in-out;
+}
+a:hover {
+  color: #f5f5f5;
+}
+.orangize {
+  display: inline-block;
+  background-image: url("../../assets/icon/orange.svg");
+  width: 30px;
+  height: 30px;
+}
+span {
+  color: #f26531;
+  font-family: Kaushan;
+}
+</style>
