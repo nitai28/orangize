c45d047e45c8a821db91adb17fd4d05af02fce5c
yotamZaken <yotamzk@gmail.com>
2018-05-07 10:59:48 +0300

    deleted remains of old taskList cmp

--- frontend/src/views/OrangizeApp.vue
+++ frontend/src/views/OrangizeApp.vue
@@ -3,17 +3,14 @@
     <section class='orangize-app'>
         <i class="orangize"></i> <h1>Orangize</h1>
         <user-msg></user-msg>
-        <task-list></task-list>
         <card-list></card-list>
     </section>
 </template>
 
 <script>
-import TaskList from "../components/task/TaskList.vue";
-import UserMsg from "../components/global/UserMsg.vue";
 
+import UserMsg from "../components/global/UserMsg.vue";
 import SocketSerivce from '../services/SocketService.js'
-import TaskList from '../components/task/TaskList.vue';
 import CardList from '../components/task/CardList.vue';
 
 export default {
@@ -26,7 +23,6 @@ export default {
     SocketSerivce.connectSocket(); 
   },
   components: {
-    TaskList,
     UserMsg,
     CardList,
   }
