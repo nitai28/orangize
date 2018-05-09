6c39bbfede1d4c46e09e2749d17a2188ee878ac6
Taytay884 <Taytay884@yahoo.com>
2018-05-09 14:46:13 +0300

    Added filter style, btn and input const classes

--- frontend/src/components/task/TaskFilter.vue
+++ frontend/src/components/task/TaskFilter.vue
@@ -1,14 +1,19 @@
 <template>
-  <section class='task-filter'>
-    <h2> Filter </h2>
-    <select :class='filter.byLabel' v-model='filter.byLabel' @change='emitFilter()'>
-        <option class='all' value=''>No Filter</option>
-        <option class='red' value='red'></option>
-        <option class='orange' value='orange'></option>
-        <option class='yellow' value='yellow'></option>
-        <option class='green' value='green'></option>
-    </select>
-    <input type='search' v-model="filter.byTitle" placeholder='Filter by title' @input="filterBySearch" />
+  <section class='task-filter flex'>
+    <form @submit.prevent="filterBySearch" class="search flex">
+      <input class="input clean-input" type='search' v-model="filter.byTitle" placeholder='Filter by title' @input="filterBySearch" />
+      <button class="btn clean-btn" type="submit" ><i><img src="../../assets/icon/search.svg"/></i></button>
+    </form>
+    <div class="btn filter-btn clean-btn" :class='filterBtnClass' @click='toggleOpts'>
+      <span>filter By Label</span>
+      <ul class="opts-container" :class="{'open-opts': isOpen}">
+        <li class='opt all' @click="filter.byLabel = ''; emitFilter()">No Filter</li>
+        <li class='opt red' @click="filter.byLabel = 'red'; emitFilter()">Immediate Attention</li>
+        <li class='opt orange' @click="filter.byLabel = 'orange'; emitFilter()">Urgent</li>
+        <li class='opt yellow' @click="filter.byLabel = 'yellow'; emitFilter()">Normal</li>
+        <li class='opt green' @click="filter.byLabel = 'green'; emitFilter()">Done</li>
+      </ul>
+    </div>
   </section>
 </template>
 
@@ -19,10 +24,22 @@ export default {
   name: "TaskFilter",
   data() {
     return {
+      isOpen: false,
       filter: { byLabel: "", byTitle: "" }
     };
   },
+  computed: {
+  filterBtnClass: function () {
+    return {
+      [this.filter.byLabel]: true,
+      'open-opts': this.isOpen
+      }
+    }
+  },
   methods: {
+    toggleOpts() {
+      this.isOpen = !this.isOpen;
+    },
     emitFilter() {
       let copyFilter = Object.assign({}, this.filter);
       console.log(copyFilter);
@@ -36,19 +53,83 @@ export default {
 </script>
 
 <style>
-.all {
+.task-filter {
+  padding: 10px;
+  width: fit-content;
+}
+.all:hover {
   background: white;
 }
-.red {
+.red:hover {
   background: rgb(197, 0, 0);
 }
-.yellow {
+.yellow:hover {
   background: rgb(240, 224, 5);
 }
-.green {
+.green:hover {
   background: rgb(35, 149, 7);
 }
-.orange {
+.orange:hover {
   background: rgb(255, 174, 0);
 }
+.opts-container.open-opts {
+  max-height: 200px;
+  opacity: 1;
+}
+.opts-container {
+  position: absolute;
+  left: 0;
+  top: 100%;
+  color: #f26531;
+  background: #231f20;
+  overflow: hidden;
+  max-height: 0px;
+  width: max-content;
+  transition: all 0.25s ease-in-out;
+  border-radius: 0px 5px 5px 5px;
+  opacity: 0;
+  box-shadow: 0px 3px 3px 0px #000000;
+}
+.opt {
+  padding: 5px;
+  transition: background 0.2s ease-in;
+}
+
+.btn {
+  position: relative;
+  width: fit-content;
+}
+
+.filter-btn span {
+  line-height: 37px;
+}
+
+.search {
+  margin-right: 20px; 
+  max-height: 38px; 
+}
+
+.search input {
+  border-radius: 5px 0px 0px 5px;
+  height: 100%;
+}
+
+.search button {
+  border-radius: 0px 5px 5px 0px;
+  height: 100%;
+}
+
+.search img {
+  height: 28px;
+  padding-top: 8px;
+}
+
+.filter-btn.open-opts {
+  border-radius: 5px 5px 0px 0px;
+}
+/*
+Black: #231f20
+LightOrange: #f26531
+WHITE: #f5f5f5
+*/
 </style>
